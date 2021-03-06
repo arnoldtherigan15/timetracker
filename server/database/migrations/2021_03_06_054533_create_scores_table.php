e24219564b575cc941878cac7e0704f0a3084118
Teddy K <teddykoerniadi@gmail.com>
2021-03-06 12:50:33 +0700

    generate module score

---
+++ server/database/migrations/2021_03_06_054533_create_scores_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateScoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scores', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('buddy_id');
+            $table->unsignedBigInteger('phase_id');
+            $table->integer('week');
+            $table->float('score');
+            $table->text('notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scores');
+    }
+}
